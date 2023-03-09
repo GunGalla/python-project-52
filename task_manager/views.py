9486199a60722a5e6763aea6dc5cffbc71f89d49
Iaroslav <GunGalla@gmail.com>
2023-03-09 12:59:04 +0200

    Add homepage and first templates

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,8 @@
+"""Views module"""
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """Home page"""
+    context = {'message': 'Hello, Hexlet!'}
+    return render(request, 'index.html', context)
