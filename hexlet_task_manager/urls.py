cc9e39ba6bac825c612f789e9649e38de04dd8e3
Iaroslav <GunGalla@gmail.com>
2023-03-10 15:06:26 +0200

    Add users page

--- hexlet_task_manager/urls.py
+++ hexlet_task_manager/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from task_manager import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('users', views.UsersView.as_view(), name='users'),
     path('admin/', admin.site.urls),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,8 +1,24 @@
 """Views module"""
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.models import User
 
 
-def index(request):
+class IndexView(View):
     """Home page"""
-    context = {'message': 'Hello, Hexlet!'}
-    return render(request, 'index.html', context)
+
+    def get(self, request, *args, **kwargs):
+        """Home page"""
+        return render(request, 'index.html')
+
+
+class UsersView(View):
+    """Urers list page"""
+
+    def get(self, request, *args, ** kwargs):
+        """Getting all registered users"""
+        users = User.objects.all()
+        if users:
+            return render(request, 'users.html', context= {'users': users})
+        else:
+            return render(request, 'users.html')
