6578b641a412e77b8f464b0f964e950629f2afbb
Iaroslav <GunGalla@gmail.com>
2023-03-21 20:07:26 +0200

    Settings changes

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -21,7 +21,7 @@ import os
 load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -145,6 +145,7 @@ LANGUAGES = [
 
 LOCALE_PATHS = [
     os.path.join(BASE_DIR, 'locale'),
+    os.path.join(BASE_DIR, 'locale/ru/LC_MESSAGES')
 ]
 
 
