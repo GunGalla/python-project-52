33f27fbf5602d12a563a6e8e3e538755ded15ced
Iaroslav <GunGalla@gmail.com>
2023-03-22 10:26:39 +0200

    Change task creation form

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,11 +1,18 @@
 """Django forms module."""
 from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django import forms
 
 from tasks.models import Task
 
 
 class TaskCreationForm(ModelForm):
     """Class to create form for user creation."""
+
+    def __init__(self, *args, **kwargs):
+        """Changing form label"""
+        super(TaskCreationForm, self).__init__(*args, **kwargs)
+        self.fields['user'].label_from_instance = lambda obj: f"{obj.first_name} {obj.last_name}"
 
     class Meta:
         """Base settings"""
