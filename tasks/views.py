6e0a9c449a666727c612b34b5e8945d2ef9b1130
Iaroslav <GunGalla@gmail.com>
2023-03-22 14:57:12 +0200

    Add bootstrap messages to base.html

--- tasks/views.py
+++ tasks/views.py
@@ -105,7 +105,7 @@ class TaskUpdateView(LoginRequiredMixin, View):
 
         if form.is_valid():
             form.save()
-            messages.success(request, _('task successfully changed'))
+            messages.success(request, 'Задача успешно изменена')
             return HttpResponseRedirect(reverse_lazy('tasks:index'))
         context = {'form': form}
         return render(request, 'tasks/task_update.html', context)
